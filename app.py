f7032fc07a8a8bda2b46952949ac6df7ab0186ad
Aditya <adityasinghak17@gmail.com>
2025-10-18 13:56:36 +0530

    feat(ui): add conditional UI rendering based on sidebar selection
    Implemented dynamic UI updates that change displayed content
    according to the selected menu option in the Streamlit sidebar.

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
 import streamlit as st
 st.sidebar.title('Flights Analytics')
-st.sidebar.selectbox('Menu',['select one','check flights','Analytics'])
+user_options = st.sidebar.selectbox('Menu',['select one','check flights','Analytics'])
+if user_options == 'check flights':
+    st.title('check Flights')
+elif user_options == 'Analytics':
+    st.title('Analytics')
+else:
+    pass
